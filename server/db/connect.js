a8633f31dbb5df7dca0d14cd0367775051794cfe
Aliyah2003 <aliyahibrahim3@gmail.com>
2024-10-16 23:32:20 +0100

    Initial commit

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,9 +1,9 @@
 //require('dotenv').config();
-//console.log(process.env)
+// console.log(process.env)
 
 const { createPool }=require('mysql2')
 const db=createPool({
-    host:process.env.HOST,
+    host:'127.0.0.1',
     user:process.env.USER1,
     password:process.env.PASSWORD,
     database:process.env.DATABASE
